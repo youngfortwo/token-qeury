e47d1624a3d2685efa67e114dc9ed0aba9612cbb
youngfortwo <younguo110@github.com>
2024-03-31 13:26:57 +0800

    add dependencies

---
+++ index.js
@@ -0,0 +1,30 @@
+var datetimeGenerator = require('tea-datetime-generator');
+
+// index.js
+
+const axios = require('axios');
+
+async function getTokenDetails(name,token, accessKey, accessSign, accessTimestamp) {
+ console.info('query start ' + name + ', now is ' + datetimeGenerator.generateFormattedDate("YYYY"))
+  try {
+    const response = await axios.get(`https://www.okx.com/api/v5/explorer/brc20/token-details?token=${token}`, {
+      headers: {
+        'OK-ACCESS-KEY': accessKey,
+        'OK-ACCESS-SIGN': accessSign,
+        'OK-ACCESS-TIMESTAMP': accessTimestamp
+      }
+    });
+    console.info('query: end ' + name + ', now is ' + datetimeGenerator.generateFormattedDate("YYYY"))
+
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching token details: ${error.message}`);
+  }
+}
+
+module.exports = getTokenDetails;
+
+
+module.exports = {
+    getTokenDetails
+}
